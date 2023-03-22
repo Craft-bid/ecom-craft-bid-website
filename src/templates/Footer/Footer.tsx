e07394ba706c0388e9536235e728838849195aa9
macszl <235989@edu.p.lodz.pl>
2023-03-22 23:59:58 +0100

    feat: added a basic version of footer

--- src/templates/Footer/Footer.tsx
+++ src/templates/Footer/Footer.tsx
@@ -1,1 +1,18 @@
-export function Footer() {}
+import { Grid, Typography } from '@mui/material';
+
+export function Footer() {
+  return (
+    <Grid
+      container
+      justifyContent={'center'}
+      alignItems={'center'}
+      height={160}
+      bgcolor={'#17252A'}
+    >
+      <Typography color={'white'}>Our links:</Typography>
+      <Typography color={'white'}>About us</Typography>
+      <Typography color={'white'}>Privacy policy:</Typography>
+      <Typography color={'white'}>Contact</Typography>
+    </Grid>
+  );
+}

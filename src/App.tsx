c78765a7bd65c9e8825d335090dacc1a21954f05
235923 <235923@edu.p.lodz.pl>
2023-04-27 17:20:34 +0200

    feat: added offerCard

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,16 @@ import { OfferPage } from './pages/OfferPage';
 import '@fontsource/montserrat';
 import '@fontsource/lato';
 import { OfferListPage } from './pages/OfferListPage';
+import { OfferCard } from './components/OfferCard/OfferCard';
+import { OfferCardProps } from './components/OfferCard/OfferCard.types';
+
+const OFFER_CARD_PROPS: OfferCardProps = {
+  image: 'https://picsum.photos/200/300',
+  title: 'CNC OPERATOR NEEDED',
+  description: 'Looking for a CNC operator with a machine to make a  metal part necessary for my  project.',
+  bids: 32,
+  avgBid: 120,
+};
 
 export function App() {
   return (
@@ -19,6 +29,10 @@ export function App() {
       <Route
         path='/offers'
         element={<OfferListPage></OfferListPage>}
+      />
+      <Route
+        path='/offer'
+        element={<OfferCard {...OFFER_CARD_PROPS} />}
       />
     </Routes>
   );
